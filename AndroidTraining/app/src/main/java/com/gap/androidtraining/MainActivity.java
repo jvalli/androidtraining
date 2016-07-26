ff6205d8db37907a176892b3722c88e56f4387ab
Jerónimo Valli <jvalli@bminds.com.ar>
2016-07-26 14:52:11 -0300

    butterknife implemented

--- AndroidTraining/app/src/main/java/com/gap/androidtraining/MainActivity.java
+++ AndroidTraining/app/src/main/java/com/gap/androidtraining/MainActivity.java
@@ -3,30 +3,27 @@ package com.gap.androidtraining;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements OnClickListener {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
-    private EditText mEditTextSearch;
-    private Button mButtonSearch;
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.edit_text_search)
+    EditText mEditTextSearch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        mEditTextSearch = (EditText)findViewById(R.id.edit_text_search);
-        mButtonSearch = (Button)findViewById(R.id.button_search);
-        assert mButtonSearch != null;
-        mButtonSearch.setOnClickListener(this);
-
+        ButterKnife.bind(this);
     }
 
-    public void onClick(View v) {
-
+    @OnClick(R.id.button_search)
+    public void onClickSearch(View view) {
         String textToast = String.format("%s '%s'", getString(R.string.searching), mEditTextSearch.getText().toString());
         Toast.makeText(getApplicationContext(), textToast, Toast.LENGTH_SHORT).show();
     }
